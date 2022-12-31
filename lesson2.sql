0e47551179925a3194a081d52d8bbeb5c30e7ad0
vulcan-332 <92639453+vulcan-332@users.noreply.github.com>
2022-12-31 11:50:39 +0530

    Create lesson2.sql

---
+++ lesson2.sql
@@ -0,0 +1,10 @@
+JOINS
+
+  SELECT orders.*
+  FROM orders
+  JOIN accounts
+  ON orders.account_id = accounts.id;
+  
+ As we've learned, the SELECT clause indicates which column(s) of data you'd like to see in the output (For Example, orders.* gives us all the columns in the orders table in the output). 
+ The FROM clause indicates the first table from which we're pulling data, and the JOIN indicates the second table. 
+ The ON clause specifies the column on which you'd like to merge the two tables together.
