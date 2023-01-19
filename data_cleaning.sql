2b47762aa7caa93fc90d114b55f0b9b54bcdb8b2
vulcan-332 <92639453+vulcan-332@users.noreply.github.com>
2023-01-19 19:30:56 +0530

    Create data_cleaning.sql

---
+++ data_cleaning.sql
@@ -0,0 +1,9 @@
+KeyTerm	      Definition
+Cast	        Converts a value of any type into a specific, different data type
+Coalesce	    Returns the first non-null value in a list
+Concat	      Adds two or more expressions together
+Left	        Extracts a number of characters from a string starting from the left
+Position	    Returns the position of the first occurrence of a substring in a string
+Right	        Extracts a number of characters from a string starting from the right
+Strpos	      Returns the position of a substring within a string
+Substr	      Extracts a substring from a string (starting at any position)
